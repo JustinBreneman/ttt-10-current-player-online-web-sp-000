782de91b15424f49c8e2b664d3c10ea76375991d
Justin Breneman <justin.breneman@yahoo.com>
2020-04-11 00:46:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even
+  if turn_count(board).even?
     "X"
-  elsif turn_count(board).odd
+  elsif turn_count(board).odd?
     "O"
   end
 end
